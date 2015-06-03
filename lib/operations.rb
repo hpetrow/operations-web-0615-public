03862f49aaedf6fb92fdfe2f0a0c3cd2ec0c8264
hpetrow <heather.petrow@gmail.com>
2015-06-03 14:37:30 -0400

    done

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,15 +1,27 @@
 def unsafe?(speed)
-
+  if (speed < 40 || speed > 60)
+    true
+  else
+    false
+  end
 end
 
 
 
 def not_safe?(speed)
-
+  (speed < 40 || speed > 60) ? true : false
 end
 
 
 
 def sum_machine(first, second)
-
+  result = 0
+  if (first == second)
+    result = first
+  elsif (first < second)
+    (first..second).each {|n| result += n}
+  else # second < first
+    (second..first).each {|n| result += n}
+  end
+  result
 end
